19f606639dbe13b32b132607b814b81c95e609a9
Jonny Vickers <jonathanbvickers@gmail.com>
2022-03-03 10:16:24 +0000

    fixing secret key

--- project/settings.py
+++ project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)@-%q@kfb7mgz=9rf#skauo8=d+4&i-=5-5*l0!pc7#3nr%%^&'
+# SECRET_KEY = 'django-insecure-)@-%q@kfb7mgz=9rf#skauo8=d+4&i-=5-5*l0!pc7#3nr%%^&'
+SECRET_KEY = os.getenv('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
