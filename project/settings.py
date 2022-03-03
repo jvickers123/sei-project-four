ee357440c2cb4c43474ce7c14bae3fb6f82fa57c
Jonny Vickers <jonathanbvickers@gmail.com>
2022-03-03 10:19:12 +0000

    removing secret key

--- project/settings.py
+++ project/settings.py
@@ -21,7 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-)@-%q@kfb7mgz=9rf#skauo8=d+4&i-=5-5*l0!pc7#3nr%%^&'
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 
