235191ccb529e7c859aa4c7c15f71c95b0b4f135
Jonny Vickers <jonathanbvickers@gmail.com>
2022-03-03 11:46:22 +0000

    created login route

--- jwt_auth/views.py
+++ jwt_auth/views.py
@@ -1,8 +1,9 @@
-from asyncio import exceptions
-from xml.dom import ValidationErr
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
+from datetime import datetime, timedelta
+import jwt
+from django.conf import settings
 
 from django.contrib.auth import get_user_model
 
@@ -32,3 +33,27 @@ class RegisterView(APIView):
         except AssertionError as e:
             return Response({ "detail": str(e)}, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
 
+class LoginView(APIView):
+
+    def post(self, request):
+        
+        try:
+            user_to_login = User.objects.get(email=request.data.get('email'))
+        except User.DoesNotExist:
+            return PermissionDenied(detail="Unauthorised")
+        
+        if not user_to_login.check_password(request.data.get('password')):
+            return PermissionDenied(detail="Unauthorised")
+
+        dt = datetime.now() + timedelta(days=7)
+
+        token = jwt.encode({
+            'sub': user_to_login.id,
+            'exp': int(dt.strftime('%s'))
+        }, settings.SECRET_KEY, 'HS256')
+
+
+        return Response({
+            'token': token,
+            'message': f"Welcome back {user_to_login.first_name}!"
+        }, status=status.HTTP_202_ACCEPTED)
