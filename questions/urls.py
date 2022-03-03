5d5434e53dc36c01a6ea488c49ec96a177d3b278
Jonny Vickers <jonathanbvickers@gmail.com>
2022-03-03 16:43:32 +0000

    added get single question and update question

--- questions/urls.py
+++ questions/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import QuestionListView
+from .views import QuestionListView, QuestionDetailView
 
 urlpatterns = [
-    path('', QuestionListView.as_view())
+    path('', QuestionListView.as_view()),
+    path('<int:pk>/', QuestionDetailView.as_view())
 ]
--- questions/views.py
+++ questions/views.py
@@ -1,6 +1,10 @@
+from functools import partial
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+
+# exceptions
+from rest_framework.exceptions import NotFound
 
 # serializers
 from .serializers.common import QuestionSerializer
@@ -14,5 +18,30 @@ class QuestionListView(APIView):
 
     def get(self, _request):
         questions = Question.objects.all()
-        serialized_questions = PopulatedQuestionSerializer(questions, many=True)
+        serialized_questions = QuestionSerializer(questions, many=True)
         return Response(serialized_questions.data, status = status.HTTP_200_OK)
+
+class QuestionDetailView(APIView):
+        
+    def get_question(self, pk):
+        try:
+            return Question.objects.get(pk=pk)
+        except Question.DoesNotExist:
+            raise NotFound(detail="Question not found.")
+
+    def get(self, _request, pk):
+        question = self.get_question(pk=pk)
+        serialized_question = PopulatedQuestionSerializer(question)
+        return Response(serialized_question.data, status=status.HTTP_200_OK)
+
+    def put(self, request, pk):
+        question = self.get_question(pk=pk)
+        serialized_question = QuestionSerializer(question, data=request.data, partial=True)
+        try:
+            serialized_question.is_valid()
+            serialized_question.save()
+            return Response(serialized_question.data, status=status.HTTP_202_ACCEPTED)
+        except AssertionError as e:
+            return Response({ "detail": str(e) }, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+        except:
+            return Response("Unprocessable Entity", status=status.HTTP_422_UNPROCESSABLE_ENTITY)
