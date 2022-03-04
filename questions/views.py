4812008d74fbc382242cb2cb799020ab7668612f
Jonny Vickers <jonathanbvickers@gmail.com>
2022-03-04 11:05:22 +0000

    created add question route

--- questions/views.py
+++ questions/views.py
@@ -1,7 +1,7 @@
-from functools import partial
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 # exceptions
 from rest_framework.exceptions import NotFound
@@ -9,6 +9,7 @@ from rest_framework.exceptions import NotFound
 # serializers
 from .serializers.common import QuestionSerializer
 from .serializers.populated import PopulatedQuestionSerializer
+from answers.serializers.common import AnswerSerializer
 
 # models
 from .models import Question
@@ -16,11 +17,62 @@ from .models import Question
 # Create your views here.
 class QuestionListView(APIView):
 
+    permission_classes = (IsAuthenticatedOrReadOnly, )
+
     def get(self, _request):
         questions = Question.objects.all()
         serialized_questions = QuestionSerializer(questions, many=True)
         return Response(serialized_questions.data, status = status.HTTP_200_OK)
-    
+
+    def post(self, request):
+        print(request.data)
+        answer_1 = request.data['answer1']
+        answer_2 = request.data['answer2']
+        question = request.data['question']
+
+        answer_1['owner'] = request.user.id
+        answer_2['owner'] = request.user.id
+        question['owner'] = request.user.id
+
+        print(question)
+
+        serialized_question = QuestionSerializer(data=question)
+
+        print(serialized_question)
+
+        try:
+
+            serialized_question.is_valid()
+            print('errrors ----->', serialized_question.errors)
+
+            serialized_question.save()
+
+            print(serialized_question.data['id'])
+
+            answer_1['question'] = serialized_question.data['id']
+            answer_2['question'] = serialized_question.data['id']
+
+            serialized_answer_1 = AnswerSerializer(data=answer_1)
+            serialized_answer_2 = AnswerSerializer(data=answer_2)
+
+            serialized_answer_1.is_valid()
+            print('errrors ----->', serialized_answer_1.errors)
+            serialized_answer_1.save()
+
+            serialized_answer_2.is_valid()
+            serialized_answer_2.save()
+
+            print(serialized_answer_1.errors, serialized_answer_2.data)
+
+            return Response(serialized_question.data, status=status.HTTP_201_CREATED)
+
+        except AssertionError as e:
+            return Response({"detail": str(e) }, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+        except KeyError as e:
+            print(e)
+            return Response({ "detail": "missing field"}, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+        except:
+            return Response({ "detail": "failed to add question"}, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
 
 class QuestionDetailView(APIView):
         
