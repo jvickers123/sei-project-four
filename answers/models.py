8ab406f2c51e5c477e3dbf9f449e2fe177b8ddb0
Jonny Vickers <jonathanbvickers@gmail.com>
2022-03-08 22:09:37 +0000

    fixed answers to make sense on profile

--- answers/models.py
+++ answers/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Answer(models.Model):
-    text = models.CharField(max_length=50, default=None)
+    text = models.CharField(max_length=100, default=None)
     question = models.ForeignKey(
         "questions.Question",
         related_name = "answers",
