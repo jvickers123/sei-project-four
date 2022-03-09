61b98905efa47735ad1d020dac2a5f6d7fafbd27
Jonny Vickers <jonathanbvickers@gmail.com>
2022-03-09 14:12:23 +0000

    get basic colors

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
-import React, { useEffect } from 'react'
-import axios from 'axios'
+import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { ChakraProvider } from '@chakra-ui/react'
 
