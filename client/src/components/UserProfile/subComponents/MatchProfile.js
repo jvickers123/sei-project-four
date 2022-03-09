304542125d08492743de51c143a317456107ad55
Jonny Vickers <jonathanbvickers@gmail.com>
2022-03-09 11:42:02 +0000

    added reject functionality to likes page

--- client/src/components/UserProfile/subComponents/MatchProfile.js
+++ client/src/components/UserProfile/subComponents/MatchProfile.js
@@ -7,29 +7,36 @@ const MatchProfile = ({ currentUser, profileId, refreshLikes }) => {
 
   const MatchProfile = async () => {
     const matchesAlready = [...currentUser.matches]
-    console.log(matchesAlready)
     const matches = matchesAlready.map(profile => profile.id)
     matches.push(profileId)
-    console.log('matches', matches)
+
     const likesAlreadyRecieved= [...currentUser.likes_recieved]
-    console.log(likesAlreadyRecieved)
     const likesRecieved = likesAlreadyRecieved.map(profile => profile.id)
-    console.log(likesRecieved)
     const indexOfMatch = likesRecieved.indexOf(profileId)
-    console.log(indexOfMatch)
     likesRecieved.splice(indexOfMatch, 1)
-    console.log(likesRecieved)
+
     const token = getTokenFromLocal()
     try {
-      const { data } = await axios.put('/api/auth/profile/', { matches: matches, likes_recieved: likesRecieved }, { headers: {Authorization: `Bearer ${token}` }} )
-      console.log(data)
+      await axios.put('/api/auth/profile/', { matches: matches, likes_recieved: likesRecieved }, { headers: {Authorization: `Bearer ${token}` }} )
       refreshLikes()
     } catch (error) {
       console.log(error.response.data.detail)
     }
   }
 
-  const rejectProfile = () => {
+  const rejectProfile = async () => {
+    const likesAlreadyRecieved= [...currentUser.likes_recieved]
+    const likesRecieved = likesAlreadyRecieved.map(profile => profile.id)
+    const indexOfMatch = likesRecieved.indexOf(profileId)
+    likesRecieved.splice(indexOfMatch, 1)
+
+    const token = getTokenFromLocal()
+    try {
+      await axios.put('/api/auth/profile/', { likes_recieved: likesRecieved }, { headers: {Authorization: `Bearer ${token}` }} )
+      refreshLikes()
+    } catch (error) {
+      console.log(error.response.data.detail)
+    }
   }
   return (
     <>
